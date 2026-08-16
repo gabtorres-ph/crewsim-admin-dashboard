cc73e87edc58a807ba8218700b3bd2efdf16b8d8
gabtorres-ph <gabtorres1011@gmail.com>
2026-08-16 20:52:00 +0800

    add user types

---
+++ src/types/user.ts
@@ -0,0 +1,24 @@
+
+export type User = {
+    id: number
+    email: string
+    currency: string
+    language: string
+    timezone: string
+}
+
+export type UserInput = {
+    email: string
+    currency: string
+    language: string
+    timezone: string
+}
+
+export type UserSortKey = 
+    | 'id'
+    | 'email'
+    | 'currency'
+    | 'language'
+    | 'timezone'
+
+export type SortDirection = 'ascending' | 'descending'
