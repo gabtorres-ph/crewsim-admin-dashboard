76951731f492f6178808d364f4a86902d3f0b444
gabtorres-ph <gabtorres1011@gmail.com>
2026-09-07 23:44:58 +0800

    wip - add types

---
+++ src/features/favorites/model/types.ts
@@ -0,0 +1,15 @@
+
+export type Favorites = {
+    id: number
+    userid: number
+    country: string
+    user: string
+}
+
+export type FavoritesCreate = {
+
+}
+
+export type FavoritesUpdate = {
+
+}
