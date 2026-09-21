6d76e790373081cf088d1dffdbde74270c01867f
gabtorres-ph <gabtorres1011@gmail.com>
2026-09-21 22:24:18 +0800

    wip - build request service for accounts

---
+++ src/app/(main)/accounts/api.ts
@@ -0,0 +1,4 @@
+
+export function fetchAccounts() {
+    return 
+}
--- src/app/(main)/accounts/page.tsx
+++ src/app/(main)/accounts/page.tsx
@@ -1,4 +1,5 @@
 import { DataTable } from "@/components/ui/data-table/DataTable"
+import { fetchAccounts } from "./api"
 
 // todo implement and wire in account schemas
 // todo implement api calls
